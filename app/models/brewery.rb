61e6f552290df97069204d58a130f5b48618505a
kaitai <kaisa.taipale@gmail.com>
2014-06-06 13:53:44 -0500

    Fix brewery name display

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -2,5 +2,5 @@ class Brewery < ActiveRecord::Base
   has_many :beers
   has_many :ratings, through: :beers
 
-  validates :brewery_name, presence: true
+  validates :name, presence: true
 end
--- app/models/brewery.rb~
+++ app/models/brewery.rb~
@@ -1,2 +1,6 @@
 class Brewery < ActiveRecord::Base
+  has_many :beers
+  has_many :ratings, through: :beers
+
+  validates :brewery_name, presence: true
 end
